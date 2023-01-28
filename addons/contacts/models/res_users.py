686e85675455c96dc98500517426a2da481c7fc5
Rachid <rachid.nassir@flyer.be>
2023-01-28 19:05:09 +0100

    best of the best

--- addons/contacts/models/res_users.py
+++ addons/contacts/models/res_users.py
@@ -17,5 +17,5 @@ class Users(models.Model):
             if activity['model'] != 'res.partner':
                 continue
             activity['icon'] = modules.module.get_module_icon('contacts')
-            print("Hellow")
+            print("Hellow Rochid you are best of the best")
         return activities
